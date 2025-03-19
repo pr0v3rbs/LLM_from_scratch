96bd1000102ab1fc0a4356bc6f10c64cb453dc3c
pr0v3rbs <pr0v3rbs@kaist.ac.kr>
2025-03-19 23:07:05 +0900

    Ready to make custom Dataset class

---
+++ ch1/A.6_Setting_up_efficient_data_loaders/main.py
@@ -0,0 +1,22 @@
+import torch
+
+X_train = torch.tensor([
+    [-1.2, 3.1],
+    [-0.9, 2.9],
+    [-0.5, 2.6],
+    [2.3, -1.1],
+    [2.7, -1.5]
+])
+y_train = torch.tensor([0, 0, 0, 1, 1])
+
+X_test = torch.tensor([
+    [-0.8, 2.8],
+    [2.6, -1.6],
+])
+y_test = torch.tensor([0, 1])
+
+print(X_train)
+print(y_train)
+
+print(X_test)
+print(y_test)
